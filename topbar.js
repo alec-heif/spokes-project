5b650dd1114060386aa8a47c07fc502403f939d0
mvernacc <mvernacc@mit.edu>
2015-05-05 23:53:30 -0400

    merge?

--- topbar.js
+++ topbar.js
@@ -1,3 +1,18 @@
 $(function(){
-  $("body").prepend('<div id="topbar"><div class="topbar_main"><a href="/spokes-project/"><div class="logo"><span class="logo_text">Spokes</span><img class="logo_image" src="content/icons/bicycle_color.png"/></div></a><span class="username"><a id="firstlogin"></a> | <a id="secondlogin"></a></span></div></div>');
+$("body").prepend(function(){/*
+<div id="topbar">
+  <div class="topbar_main">
+    <a href="/spokes-project/">
+      <div class="logo">
+        <span class="logo_text">Spokes</span>
+        <img class="logo_image" src="content/icons/bicycle_color.png"/>
+      </div>
+    </a>
+    <span class="username">
+      <a id="firstlogin"></a> | <a id="secondlogin"></a>
+    </span>
+  </div>
+</div>
+*/}.toString().slice(14,-3));
 });
+
